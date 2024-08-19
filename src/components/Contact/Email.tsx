fc1432d435ca5497a0b9f6f6d6b92b9c71f3be8c
alexanderpoge <flekssando@mail.ru>
2024-08-19 21:38:50 +0300

    Add: Email components

--- src/components/Contact/Email.tsx
+++ src/components/Contact/Email.tsx
@@ -1,5 +1,5 @@
 import emails from '@emailjs/browser';
-import { FormEvent, useRef } from 'react';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 
 interface EmailProps {
   name: string;
@@ -8,18 +8,34 @@ interface EmailProps {
   subject: string;
 }
 
-export const EmailContact = ({ name, email, message, subject }: EmailProps) => {
-  const form = useRef(null!);
+export const EmailContact = () => {
+  const [formValue, setFormValue] = useState<EmailProps>({
+    name: '',
+    email: '',
+    message: '',
+    subject: '',
+  });
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, type: string) => {
+    setFormValue((prevState) => ({ ...prevState, [type]: e.target.value }));
+  };
+  const form = useRef(null);
   const sendEmail = (e: FormEvent) => {
+    const { name, email, message, subject } = formValue;
     e.preventDefault();
 
     emails
-      .sendForm('service_a44s0ko', 'template_m9gottz', form.current, {
-        publicKey: 'nATcz3pWSN-YA9fs7',
-      })
+      .send(
+        'service_a44s0ko',
+        'template_m9gottz',
+        { name, email, message, subject },
+        {
+          publicKey: 'nATcz3pWSN-YA9fs7',
+        },
+      )
       .then(
         () => {
           console.log('SUCCESS!');
+          console.log(form.current);
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -43,26 +59,30 @@ export const EmailContact = ({ name, email, message, subject }: EmailProps) => {
           <input
             className="border w-[434px] h-[49px] focus:outline-none rounded-[8px] pl-4"
             type="text"
-            value={name}
+            value={formValue.name}
+            onChange={(e) => handleChange(e, 'name')}
             placeholder="Template Cookie"
           />
           <input
             className="border w-[434px] h-[49px] focus:outline-none rounded-[8px] pl-4"
             type="email"
-            value={email}
+            value={formValue.email}
+            onChange={(e) => handleChange(e, 'email')}
             placeholder="Template Cookie"
           />
         </div>
         <input
           className="border w-[884px] h-[49px] focus:outline-none rounded-[8px] pl-4"
           type="name"
-          value={message}
+          value={formValue.message}
+          onChange={(e) => handleChange(e, 'message')}
           placeholder="Message"
         />
         <input
           className="border w-[884px] h-[98px] focus:outline-none rounded-[8px] pl-4 pb-12 mt-4"
           type="name"
-          value={subject}
+          value={formValue.subject}
+          onChange={(e) => handleChange(e, 'subject')}
           placeholder="Subjects"
         />
         <button className="w-[199px] h-[51px] bg-[#00B307] hover:bg-[#2C742F] rounded-full mt-6">
